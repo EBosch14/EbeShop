4cb3e79b96552ca51b728fd610a2740f128fbd6a
Enzo Bosch <enzo.jbosch@gmail.com>
2023-03-02 18:16:56 -0300

    someone features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import FilterCategory from "./assets/components/FilterCategory";
 import ProductsList from "./assets/components/ProductsList";
 
 export default function App() {
-
   return (
     <>
-      <h1 className="container text-5xl mx-auto mt-8">EbeShop</h1>
+      <header>
+        <h1 className="container text-5xl mx-auto mt-8">EbeShop</h1>
+        <FilterCategory></FilterCategory>
+      </header>
       <ProductsList></ProductsList>
     </>
   );
